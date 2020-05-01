a0de670e5fe3f70b5eb5bff2082951a4767cddde
Dane Kennedy <kennedy.dane@gmail.com>
2020-05-01 15:21:44 +0200

    There's more than one MPI in town

--- ansible/roles/common/templates/openmpi.lua
+++ ansible/roles/common/templates/openmpi.lua
@@ -13,7 +13,6 @@ local include_dir = pathJoin(mpi_dir, "include")
 local man_dir = pathJoin(mpi_dir, "share/man")
 local pkgconfig_dir = pathJoin(mpi_dir, "lib/pkgconfig")
 
-
 prepend_path('PATH', bin_dir)
 prepend_path('LD_LIBRARY_PATH', lib_dir)
 prepend_path('INCLUDE', include_dir)
