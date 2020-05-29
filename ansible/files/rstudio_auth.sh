070656dfb1084268efcd60ffdd07d66238192e50
Dane Kennedy <kennedy.dane@gmail.com>
2020-05-29 18:57:30 +0200

    [WIP] Build RStudio container

---
+++ ansible/files/rstudio_auth.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Thanks Nickjer! https://github.com/nickjer/singularity-rstudio/blob/master/Singularity.3.6.0
+
+# Confirm username is supplied
+if [[ $# -ne 1 ]]; then
+  echo "Usage: auth USERNAME"
+  exit 1
+fi
+USERNAME="${1}"
+
+# Confirm password environment variable exists
+if [[ -z "${RSTUDIO_PASSWORD}" ]]; then
+  echo "The environment variable RSTUDIO_PASSWORD is not set"
+  exit 1
+fi
+
+# Read in the password from user
+read -s -p "Password: " PASSWORD
+echo ""
+
+# Authenticate user
+if [[ "${USER}" == "${USERNAME}" && "${PASSWORD}" == "${RSTUDIO_PASSWORD}" ]]; then
+  echo "Successful authentication"
+  exit 0
+else
+  echo "Invalid authentication"
+  exit 1
+fi
