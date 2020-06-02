b4b521c069277e8967ccbea48136a4f88284ecbc
Dane Kennedy <kennedy.dane@gmail.com>
2020-06-02 17:20:08 +0200

    Add PopGen container, and module file for RStudio

---
+++ ansible/roles/containers/templates/RStudio.lua
@@ -0,0 +1,22 @@
+-- -*- lua -*-
+help([[
+This module configures {{ container_name }}
+]])
+whatis("Version: item.rstudio_version")
+whatis("Keywords: R, RStudio, Rscript")
+whatis("Description: RStudio container")
+
+local singularity_image = "{{ container_image }}"
+
+setenv ("R_INSTALL_STAGED" ,"false")
+
+apps = {
+  "R",
+  "Rscript",
+  "rstudio",
+}
+
+for i, app in ipairs(apps) do
+    set_alias(app, "singularity run --app " .. app .. " " .. singularity_image)
+end
+
