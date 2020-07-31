a6851acaf5f195ac5ba01db9598c8c2268ef1832
Dane Kennedy <kennedy.dane@gmail.com>
2020-07-31 10:27:12 +0200

    Install cuda

---
+++ ansible/roles/compiled/templates/cuda.lua
@@ -0,0 +1,22 @@
+-- -*- lua -*-
+help([[
+This module configures cuda {{ version_number }} for use
+]])
+whatis("Version: {{ version_number }}")
+whatis("Keywords: cuda")
+whatis("Description: cuda {{ version_number }}")
+
+local cuda_dir = "{{ install_dir }}"
+local bin_dir = pathJoin(cuda_dir, "bin")
+local lib_dir = pathJoin(cuda_dir, "lib64")
+local include_dir = pathJoin(cuda_dir, "include")
+local man_dir = pathJoin(cuda_dir, "doc/man")
+local pkgconfig_dir = pathJoin(cuda_dir, "pkgconfig")
+
+prepend_path('PATH', bin_dir)
+prepend_path('LD_LIBRARY_PATH', lib_dir)
+prepend_path('LIBRARY_PATH', lib_dir)
+prepend_path('INCLUDE', include_dir)
+prepend_path('C_INCLUDE', include_dir)
+prepend_path('MANPATH', man_dir)
+prepend_path('PKG_CONFIG_PATH', pkgconfig_dir)
