a6f8fcb0e8f05b55a3bf72127e5e0c0c0b50d1ef
Dane Kennedy <kennedy.dane@gmail.com>
2020-07-31 10:37:11 +0200

    Add CUDA_PATH

--- ansible/roles/compiled/templates/cuda.lua
+++ ansible/roles/compiled/templates/cuda.lua
@@ -13,6 +13,7 @@ local include_dir = pathJoin(cuda_dir, "include")
 local man_dir = pathJoin(cuda_dir, "doc/man")
 local pkgconfig_dir = pathJoin(cuda_dir, "pkgconfig")
 
+setenv('CUDA_PATH', cuda_dir)
 prepend_path('PATH', bin_dir)
 prepend_path('LD_LIBRARY_PATH', lib_dir)
 prepend_path('LIBRARY_PATH', lib_dir)
