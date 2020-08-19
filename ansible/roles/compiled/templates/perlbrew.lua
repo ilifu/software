bac2760c4d55beb0668b1e2626251ff01fe3bdce
Dane Kennedy <kennedy.dane@gmail.com>
2020-08-19 20:06:20 +0200

    manual perlbrew

--- ansible/roles/compiled/templates/perlbrew.lua
+++ ansible/roles/compiled/templates/perlbrew.lua
@@ -6,7 +6,10 @@ whatis("Keywords: perlbrew")
 whatis("Description: perlbrew manages perl installations. Note, you cannot unload this module in a session!")
 
 local perlbrew_dir = "{{ install_dir }}"
+local bin_dir = pathJoin(perlbrew_dir, "bin")
+local home_dir = os.getenv ( "HOME" )
+local perlbrew_home = pathJoin(home_dir, '.perlbrew')
 
-if (mode() == "load") then
-   io.stdout:write("source {{ install_dir }}/etc/bashrc\n")
-end
+setenv('PERLBREW_ROOT', perlbrew_dir)
+setenv('PERLBREW_HOME', perlbrew_home)
+prepend_path('PATH', bin_dir)
