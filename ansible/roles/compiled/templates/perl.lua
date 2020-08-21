d3a06b0d369b92cc98f08bbae008afbac44e14c6
Dane Kennedy <kennedy.dane@gmail.com>
2020-08-21 07:18:16 +0200

    Install perl

---
+++ ansible/roles/compiled/templates/perl.lua
@@ -0,0 +1,16 @@
+-- -*- lua -*-
+help([[
+This module configures perl {{ version_number }} for use
+]])
+whatis("Keywords: perl")
+whatis("Version: {{ version_number }}")
+whatis("Description: Perl {{ version_number }}")
+
+local perl_dir = "{{ install_dir }}"
+local bin_dir = pathJoin(perl_dir, "bin")
+local lib_dir = pathJoin(perl_dir, "lib")
+local man_dir = pathJoin(perl_dir, "man")
+
+prepend_path('PATH', bin_dir)
+prepend_path('LD_LIBRARY_PATH', lib_dir)
+prepend_path('LIBRARY_PATH', lib_dir)
