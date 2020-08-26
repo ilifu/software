33f713def0f7785a8c05080e17156a870c880fb7
Dane Kennedy <kennedy.dane@gmail.com>
2020-08-26 12:57:53 +0200

    Install homebrew

--- ansible/roles/compiled/templates/homebrew.lua
+++ ansible/roles/compiled/templates/homebrew.lua
@@ -8,10 +8,11 @@ whatis("Description: Use homebrew to manage software packages")
 local homebrew_dir = "{{ install_dir }}"
 local bin_dir = pathJoin(homebrew_dir, "bin")
 local man_dir = pathJoin(homebrew_dir, "manpages")
+local software_dir = pathJoin("{{ homebrew_dir }}", "software")
 
 load("ruby/2.6")
 
-setenv('HOMEBREW_PREFIX', homebrew_dir)
-setenv('HOMEBREW_PREFIX_DEFAULT', homebrew_dir)
+setenv('HOMEBREW_PREFIX', software_dir)
+setenv('HOMEBREW_PREFIX_DEFAULT', software_dir)
 prepend_path('PATH', bin_dir)
 prepend_path('MANPATH', man_dir)
