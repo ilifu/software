06c06b20e541d36a1beb1c7b5891bcbd22402ffd
Dane Kennedy <kennedy.dane@gmail.com>
2020-09-30 12:39:39 +0200

    Add filter_vep alias

--- ansible/roles/containers/templates/vep.lua
+++ ansible/roles/containers/templates/vep.lua
@@ -10,5 +10,6 @@ local singularity_image = "{{ container_image }}"
 setenv ("SINGULARITY_BINDPATH" ,"{{ data_dir }}:{{ cache_dir }}")
 
 set_alias("vep", "singularity exec " .. singularity_image .. " vep --cache --dir_cache {{ cache_dir }} --dir_plugins {{ cache_dir }}")
+set_alias("filter_vep", "singularity exec " .. singularity_image .. " filter_vep")
 
 
