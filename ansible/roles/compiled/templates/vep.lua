d8fb9afda437e0ccf1bd351580f4e9f4f831ea0f
Dane Kennedy <kennedy.dane@gmail.com>
2020-10-21 14:46:06 +0200

    Fix vep module

--- ansible/roles/compiled/templates/vep.lua
+++ ansible/roles/compiled/templates/vep.lua
@@ -11,5 +11,6 @@ load("{{ perl_module }}")
 load("htslib/1.10.2")
 
 prepend_path("PERL5LIB", "{{ install_dir }}")
+prepend_path("PATH", "{{ install_dir }}")
 
-
+set_alias("vep", "vep --cache --dir_cache {{ cache_dir }} --dir_plugins {{ cache_dir }}/Plugins")
