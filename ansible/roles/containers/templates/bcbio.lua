19f4d8dae5587bafef35a840a355182b0a5d9af1
Dane Kennedy <kennedy.dane@gmail.com>
2022-03-14 10:59:25 +0200

    bind mount cosmic shiznizz

--- ansible/roles/containers/templates/bcbio.lua
+++ ansible/roles/containers/templates/bcbio.lua
@@ -8,5 +8,6 @@ whatis("Description: alidated, scalable, community developed variant calling, RN
 local singularity_image = "{{ container_image }}"
 
 -- setenv ("SINGULARITY_BIND", "/software/bio/bcbio_genomes:/opt/bcbio_nextgen/genomes")
+setenv ("SINGULARITY_BIND", "/software/bio/bcbio_genomes/Hsapiens/hg38/variation/cosmic-v91.vcf.gz:/opt/bcbio_nextgen/genomes/Hsapiens/hg38/variation/cosmic-v91.vcf.gz,/software/bio/bcbio_genomes/Hsapiens/hg38/variation/cosmic-v91.vcf.gz.tbi:/opt/bcbio_nextgen/genomes/Hsapiens/hg38/variation/cosmic-v91.vcf.gz.tbi,/software/bio/bcbio_genomes/Hsapiens/hg38/variation/cosmic.vcf.gz:/opt/bcbio_nextgen/genomes/Hsapiens/hg38/variation/cosmic.vcf.gz,/software/bio/bcbio_genomes/Hsapiens/hg38/variation/cosmic.vcf.gz.tbi:/opt/bcbio_nextgen/genomes/Hsapiens/hg38/variation/cosmic.vcf.gz.tbi")
 
 set_alias("bcbio_nextgen.py", "singularity exec {{ container_image }} bcbio_nextgen.py")
