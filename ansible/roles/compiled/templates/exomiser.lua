f410268b84c42994cb7d103d426544e1efeeb56e
Dane Kennedy <kennedy.dane@gmail.com>
2022-06-14 09:53:00 +0200

    get exomiser data and create module

---
+++ ansible/roles/compiled/templates/exomiser.lua
@@ -0,0 +1,17 @@
+-- -*- lua -*-
+help([[
+This module configures exomiser {{ version_number }} for use
+]])
+whatis("Version: {{ version_number }}")
+whatis("Keywords: exomiser")
+whatis("Description: exomiser {{ version_number }}")
+
+load("java/openjdk-17.0.2")
+
+local exomiser_dir = "{{ install_dir }}"
+
+setenv('_JAVA_OPTIONS', '-Xmx16g')
+
+prepend_path('PATH', exomiser_dir)
+prepend_path('CLASSPATH', exomiser_dir)
+
