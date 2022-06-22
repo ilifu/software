0335ce60ad5025d4ea01311da36c20be8b95a569
Dane Kennedy <kennedydane@users.noreply.github.com>
2022-06-22 10:35:35 +0200

    Dane/fix/clin sv (#33)
    * fix permissions
    
    * fix module
    
    * Only fetch data if strictly necessary

--- ansible/roles/containers/templates/ClinSV.lua
+++ ansible/roles/containers/templates/ClinSV.lua
@@ -5,4 +5,4 @@ This module configures the ClinSV container
 whatis("Keywords: ClinSV")
 whatis("Description: ClinSV")
 
-set_alias("ClinSV", "singularity exec {{ container_image }}")
+set_alias("ClinSV", "singularity run --bind /software:/software {{ container_image }}")
