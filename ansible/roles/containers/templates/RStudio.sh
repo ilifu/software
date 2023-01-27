b6c06e6b351e114c5449fe308ffc6b868b7050de
Dane Kennedy <kennedy.dane@gmail.com>
2023-01-27 16:50:21 +0200

    Update RStudio to work with newer RStudios

--- ansible/roles/containers/templates/RStudio.sh
+++ ansible/roles/containers/templates/RStudio.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-singularity run --app RStudio --bind /software:/software {{ container_image }} "$@"
+SERVER_DIR="${HOME}/.RStudio.rserver"
+
+if [ ! -d ${SERVER_DIR}/data ]; then
+    mkdir -p "${SERVER_DIR}/data" || { echo "Could not create ${SERVER_DIR}, aborting..."; exit 1; }
+  fi
+  if [ ! -f "${SERVER_DIR}/var/lib" ]; then
+    mkdir -p "${SERVER_DIR}/var/lib" || { echo "Could not create ${SERVER_DIR}, aborting..."; exit 1; }
+  fi
+
+singularity run --app rstudio --bind /software:/software --bind "${SERVER_DIR}/var/lib":/var/lib/rstudio-server {{ container_image }} "$@"
