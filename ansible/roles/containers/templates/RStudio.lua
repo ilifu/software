6feb4a23b41bff08ac2e160461181e8abc4b4bf3
Dane Kennedy <kennedydane@users.noreply.github.com>
2023-01-27 16:52:19 +0200

    Update RStudio. Add Gnu Scientific Library. (#50)
    * Update RStudio. Add Gnu Scientific Library.
    
    * Update container to work with newer RStudios

--- ansible/roles/containers/templates/RStudio.lua
+++ ansible/roles/containers/templates/RStudio.lua
@@ -10,15 +10,5 @@ local singularity_image = "{{ container_image }}"
 
 setenv ("R_INSTALL_STAGED" ,"false")
 
-apps = {
-  "R",
-  "Rscript",
-  "rstudio",
-}
-
-for i, app in ipairs(apps) do
-    set_alias(app, "singularity run --app " .. app .. " " .. singularity_image)
-end
-
 prepend_path("PATH", "{{ R_binary_dir }}")
 
