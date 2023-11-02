83b6aa286a3e2655938740c4b87e57d12965d1b3
Dane Kennedy <kennedy.dane@gmail.com>
2023-11-02 17:52:07 +0200

    dumbass

--- ansible/roles/compiled/templates/cuda.lua
+++ ansible/roles/compiled/templates/cuda.lua
@@ -19,7 +19,7 @@ local cupti_include = pathJoin(cupti_dir, "include")
 
 setenv('CUDA_PATH', cuda_dir)
 setenv('CUDA_DIR', cuda_dir)
-setenv('XLA_FLAGS=', cuda_dir)
+setenv('XLA_FLAGS', cuda_dir)
 
 prepend_path('PATH', bin_dir)
 prepend_path('LD_LIBRARY_PATH', lib_dir)
