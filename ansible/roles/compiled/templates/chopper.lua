5a4759ec125b81af98586a2f42f0285cb725090f
Dane Kennedy <kennedy.dane@gmail.com>
2023-11-13 13:11:14 +0200

    install chopper

---
+++ ansible/roles/compiled/templates/chopper.lua
@@ -0,0 +1,10 @@
+-- -*- lua -*-
+help([[
+This module configures chopper for use
+]])
+whatis("Keywords: chopper")
+whatis("Description: chopper")
+whatis("Version: {{ version_number }}")
+
+prepend_path('PATH', "{{ install_dir }}")
+
