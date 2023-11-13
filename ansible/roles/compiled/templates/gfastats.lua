e226e0c1404575d4cc84ab832c8eba190bc2cc36
Dane Kennedy <kennedy.dane@gmail.com>
2023-11-13 12:37:54 +0200

    Install gfastats

---
+++ ansible/roles/compiled/templates/gfastats.lua
@@ -0,0 +1,10 @@
+-- -*- lua -*-
+help([[
+This module configures gfastata for use
+]])
+whatis("Keywords: gfastata")
+whatis("Description: gfastata")
+whatis("Version: {{ version_number }}")
+
+prepend_path('PATH', "{{ install_dir }}")
+
