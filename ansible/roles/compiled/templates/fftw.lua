9f0530c022c856ff4833d91741ab21ca0d61e7ed
Dane Kennedy <kennedy.dane@gmail.com>
2023-11-29 17:00:00 +0200

    Install fftw

---
+++ ansible/roles/compiled/templates/fftw.lua
@@ -0,0 +1,16 @@
+-- -*- lua -*-
+help([[
+This module configures fftw {{ version_number }} for use
+]])
+whatis("Version: {{ version_number }}")
+whatis("Keywords: fftw")
+whatis("Description: fftw {{ version_number }}")
+
+prepend_path('PATH', "{{ install_dir }}/bin")
+prepend_path('LD_LIBRARY_PATH', "{{ install_dir }}/lib")
+prepend_path('LIBRARY_PATH', "{{ install_dir }}/lib")
+prepend_path('INCLUDE', "{{ install_dir }}/include")
+prepend_path('C_INCLUDE', "{{ install_dir }}/include")
+prepend_path('CPATH', "{{ install_dir }}/include")
+prepend_path('MANPATH', "{{ install_dir }}/share/man")
+prepend_path('PKG_CONFIG_PATH', "{{ install_dir }}/lib/pkgconfig")
