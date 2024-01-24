b8630436cb361fc745c8725b12b9b3a1b582cc76
Dane Kennedy <kennedy.dane@gmail.com>
2024-01-24 13:40:16 +0200

    [WIP]

---
+++ ansible/roles/compiled/templates/common/intel.lua
@@ -0,0 +1,19 @@
+-- -*- lua -*-
+help([[
+This module configures Intel Compiler and Libraries ({{ major_minor }}) for use
+]])
+whatis("Version: {{ version }}")
+whatis("Keywords: Intel, MKL, HPC")
+whatis("Description: Intel oneAPI and HPC toolkits {{ major_minor }}")
+
+------------------------------------------------------------------------
+-- Thanks @ https://github.com/TACC/Lmod/blob/main/rt/hook/mf/Core/intel/intel.lua
+------------------------------------------------------------------------
+
+prepend_path('PATH', '{{ install_dir }}/{{ major_minor }}/bin')
+prepend_path('LIBRARY_PATH', '{{ install_dir }}/{{ major_minor }}/lib32')
+prepend_path('LIBRARY_PATH', '{{ install_dir }}/{{ major_minor }}/lib')
+prepend_path('PKG_CONFIG_PATH', '{{ install_dir }}/{{ major_minor }}/lib/pkgconfig')
+
+prepend_path('MANPATH', '{{ install_dir }}/{{ major_minor }}/share/man')
+prepend_path('INFOPATH', '{{ install_dir }}/{{ major_minor }}/share/info')
