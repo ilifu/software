d9533189d63fa568fd5ec2e4821ce1fcbbce87b5
Dane Kennedy <kennedy.dane@gmail.com>
2024-02-27 12:56:06 +0200

    fix whatis

--- ansible/roles/compiled/templates/bio/gemma.lua
+++ ansible/roles/compiled/templates/bio/gemma.lua
@@ -3,7 +3,7 @@ help([[
 This module configures gemma {{ version_number }}
 ]])
 whatis("Version: {{ version_number }}")
-whatis("Keywords: blast")
+whatis("Keywords: gemma")
 whatis("Description: gemma {{ version_number }}")-------------------------------------------
 
 prepend_path('PATH', '{{ install_dir }}')
--- ansible/roles/compiled/templates/bio/hapgen2.lua
+++ ansible/roles/compiled/templates/bio/hapgen2.lua
@@ -3,7 +3,7 @@ help([[
 This module configures HAPGEN2 {{ version_number }}
 ]])
 whatis("Version: {{ version_number }}")
-whatis("Keywords: blast")
+whatis("Keywords: HAPGEN2")
 whatis("Description: HAPGEN2 {{ version_number }}")-------------------------------------------
 
 prepend_path('PATH', '{{ install_dir }}')
