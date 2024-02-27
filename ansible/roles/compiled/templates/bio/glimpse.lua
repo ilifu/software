cf6920fddfb7eaa51b210f8aaf80b4e75c053468
Dane Kennedy <kennedy.dane@gmail.com>
2024-02-27 14:42:26 +0200

    GLIMPSE is as glimpse does

--- ansible/roles/compiled/templates/bio/glimpse.lua
+++ ansible/roles/compiled/templates/bio/glimpse.lua
@@ -1,9 +1,9 @@
 -- -*- lua -*-
 help([[
-This module configures glimpse {{ version_number }}
+This module configures GLIMPSE {{ version_number }}
 ]])
 whatis("Version: {{ version_number }}")
-whatis("Keywords: shapeit5")
-whatis("Description: shapeit5 {{ version_number }}")-------------------------------------------
+whatis("Keywords: GLIMPSE")
+whatis("Description: GLIMPSE {{ version_number }}")-------------------------------------------
 
 prepend_path('PATH', '{{ install_dir }}')
