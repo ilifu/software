74ba1bcecf950dd5bd99eb6bd2ba71a0c4f25a7c
Dane Kennedy <kennedy.dane@gmail.com>
2024-08-14 12:51:21 +0200

    fix salmon lua???

--- ansible/roles/compiled/templates/bio/salmon.lua
+++ ansible/roles/compiled/templates/bio/salmon.lua
@@ -6,7 +6,6 @@ whatis("Version: {{ version_number }}")
 whatis("Keywords: Salmon")
 whatis("Description: Salmon {{ version_number }}")
 
+prepend_path('PATH', '{{install_dir}}/bin')
 prepend_path('LD_LIBRARY_PATH', '{{install_dir}}/lib')
 prepend_path('LIBRARY_PATH', '{{install_dir}}/lib')
-
-
