fda352d2d6dd516df450d5f1c4cc6c24b743d270
Dane Kennedy <kennedy.dane@gmail.com>
2025-09-16 10:45:22 +0200

    fix data dir

--- ansible/roles/compiled/templates/bio/vip.lua
+++ ansible/roles/compiled/templates/bio/vip.lua
@@ -10,4 +10,4 @@ depends_on("apptainer/1.4.2", "java/openjdk-22.0.1")
 
 prepend_path("PATH", "{{ install_dir }}")
 setenv("VIP_DIR", "{{ install_dir }}")
-setenv("VIP_DIR_DATA", "/software/data/bio/vip/{{ version_number }}")
+setenv("VIP_DIR_DATA", "{{ vip_data_dir }}")
